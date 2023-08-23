35c4bad682998b1813815f8859c49af649c763f1
Aidan Bongiorno <aidanbongiorno@gmail.com>
2023-08-23 14:34:57 -0400

    check it

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,8 @@ function App() {
   const selectRef = useRef<HTMLSelectElement>(null!);
   const [filebase64, setFileBase64] = useState<string>("");
   const [fileRef, setFileRef] = useState<File | null>(null);
-  const [base64DataUrl, setBase64DataUrl] = useState<string | null>(null); // Declare base64DataUrl at a higher scope
+  const [base64DataUrl, setBase64DataUrl] = useState<string>(""); // Initialize with an empty string
+  const [imageElement, setImageElement] = useState<HTMLImageElement>();
 
   // ...
 
@@ -64,35 +65,25 @@ function App() {
         const base64DataUrl = ev.target.result;
         console.log(base64DataUrl);
 
-        // Display the Base64 Data URL in an image tag
-        const imgElement = document.createElement("img");
-        imgElement.src = base64DataUrl;
-        document.body.appendChild(imgElement); // You can append it to any HTML element
-        setBase64DataUrl(base64DataUrl); // Update the global variable
-      };
+        if (base64DataUrl) {
+          //* We can style an imgElement to match canvas/ video stream
+          const imgElement = document.createElement("img");
+          imgElement.src = base64DataUrl;
+          document.body.appendChild(imgElement); // You can append it to any HTML element
+          imgElement.style.width = "500px";
+          imgElement.style.height = "300px";
+          imgElement.style.border = "solid";
+          imgElement.style.position = "absolute";
+          imgElement.style.top = "25px";
+          imgElement.style.left = "0px";
 
+          setImageElement(imgElement);
+          setBase64DataUrl(base64DataUrl);
+        }
+      };
       reader.readAsDataURL(fileRef);
     }
   }
-
-  const upload = async () => {
-    // File object is already a Blob
-    let final = base64DataUrl?.replace("data:image/jpeg;base64,", "");
-    final = final?.replace("data:image/png;base64,", "");
-    const body = JSON.stringify({ image_data: [final] });
-
-    const b64 = base64DataUrl;
-    const url = "http://10.27.3.67/predict";
-    const response = await fetch(url, {
-      method: "post",
-      body: body,
-      headers: { "Content-Type": "application/json" },
-    });
-    const data = await response.json();
-    setLastPrediction(data as PredictionResult);
-
-    setTimeout(() => draw(), 100);
-  };
 
   const start = async () => {
     const idx = selectRef.current.selectedIndex;
@@ -105,8 +96,8 @@ function App() {
       const stream = await navigator.mediaDevices.getUserMedia({
         video: {
           deviceId: device.deviceId,
-          width: { ideal: 640 },
-          height: { ideal: 480 },
+          width: { ideal: 300 },
+          height: { ideal: 200 },
         },
       });
       streamRef.current = stream;
@@ -158,12 +149,35 @@ function App() {
 
     setTimeout(() => draw(), 100);
   };
+  const upload = async () => {
+    // File object is already a Blob
+    let final = base64DataUrl?.replace("data:image/jpeg;base64,", "");
+    final = final?.replace("data:image/png;base64,", "");
+    const body = JSON.stringify({ image_data: [final] });
+    const b64 = base64DataUrl;
+    const url = "http://10.27.3.67/predict";
+    const response = await fetch(url, {
+      method: "post",
+      body: body,
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await response.json();
+    setLastPrediction(data as PredictionResult);
+    setTimeout(() => draw(), 100);
+  };
 
   const save = () => {
     const filename = lastPrediction.timestamp_start + ".jpg";
     saveAs(lastBlob, filename);
   };
+  const predict = () => {
+    if (!imageElement || !drawCanvasRef.current) return;
 
+    const imgSrc = imageElement.src;
+    const canvas = drawCanvasRef.current;
+
+    drawImageOnCanvas(imgSrc, canvas);
+  };
   const draw = () => {
     if (!drawCanvasRef.current) return;
     if (!lastBlob) return;
@@ -186,12 +200,47 @@ function App() {
               ctx.strokeRect(l, t, box.w, box.h);
 
               ctx.strokeText(box.class_name, l, t);
+              const canvas = drawCanvasRef.current;
+              drawImageOnCanvas(base64DataUrl, canvas);
             }
           });
         });
       };
     }
   };
+
+  function drawImageOnCanvas(imageSrc: string, canvas: HTMLCanvasElement) {
+    canvas.style.width = "500px";
+    canvas.style.height = "300px";
+    canvas.style.border = "solid";
+    canvas.style.position = "inline";
+    canvas.style.marginLeft = "300px";
+    canvas.style.padding = "0px";
+
+    const ctx = canvas.getContext("2d");
+
+    if (ctx) {
+      const img = new Image();
+      img.src = imageSrc;
+      img.onload = () => {
+        console.log("Drawing image");
+        ctx.drawImage(img, 0, 0);
+
+        lastPrediction.predictions.forEach((prediction) => {
+          prediction.boxes.forEach((box) => {
+            if (box.class_name !== "placeholder_form") {
+              ctx.strokeStyle = "black";
+              const l = box.x_center - box.w / 2;
+              const t = box.y_center - box.h / 2;
+              ctx.strokeRect(l, t, box.w, box.h);
+
+              ctx.strokeText(box.class_name, l, t);
+            }
+          });
+        });
+      };
+    }
+  }
 
   return (
     <div className="App">
@@ -201,8 +250,10 @@ function App() {
             <option key={dev.deviceId}>{dev.label}</option>
           ))}
         </select>
-        <button onClick={start}>{isCapturing ? "Stop" : "Start"}</button>
-        <button onClick={capture}>Capture</button>
+        <button onClick={start}>{isCapturing ? "Stop" : "Start"} </button>
+        <button onClick={capture} disabled={!isCapturing}>
+          Capture
+        </button>
 
         <button onClick={save} disabled={lastBlob === null}>
           Save
@@ -210,6 +261,9 @@ function App() {
 
         <button onClick={draw} disabled={lastBlob === null}>
           Draw
+        </button>
+        <button onClick={predict} disabled={!base64DataUrl}>
+          Predict
         </button>
         <button onClick={upload}> Upload</button>
         <input type="file" onChange={(e) => convertFile(e.target.files)} />
